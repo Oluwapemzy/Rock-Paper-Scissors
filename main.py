1a381071ded622fe189e803266dabb5d0012cdfd
Oluwapemzy <ojeksman2000@gmail.com>
2022-06-03 03:45:49 +0100

    rock scissors paper done just remains to make it display what they picked not letter

--- main.py
+++ main.py
@@ -2,12 +2,22 @@ import random
 
 
 my_list = ["R", "P", "S"]
-user_option = input("Pick an option between R, P or S\n")
+user_option = input(
+    "Pick an option between \nR===> Rock, \nP===> Paper \nS ===> Scissors\n")
 while True:
-    if user_option not in my_list:
+    if user_option.upper() not in my_list:
         print("!!!Invalid\nTry Again")
         user_option = input("Pick an option between R, P or S\n")
     else:
-        cpu_option = random.choice(my_list)
-        print(f"Player {user_option}: CPU ({cpu_option})")
+        computer = random.choice(my_list)
+        print(f"Player {user_option}: CPU ({computer})")
+        if (user_option.upper() == "P" and computer =="R") or (user_option.upper() == "S" and computer=="P") or (user_option.upper() == "R" and computer=="S"):
+            print("Player, You win")
+            break
+        elif user_option.upper() == computer:
+            print("Its a tie, continue to know who wins")
+            user_option = input("Pick an option between R, P or S\n")
+            continue
+        else:
+            print("Player, You lose")
         break
